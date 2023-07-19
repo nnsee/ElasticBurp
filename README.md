6996450da8b808a3d12b17209977b4e3c843ec5e
Rasmus Moorats <xx@nns.ee>
2023-07-19 17:42:12 +0300

    Revert "create TPM2 PCR signature"
    This reverts commit 7b1f4497b4dd41f39feffd2a30355e6b31d800a6.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### Description
 
-This script (`create-unified-kernel.sh`) combines the Linux kernel and initrd images and creates a unified image which is then signed for secure boot. `systemd-measure` is used in order to sign a TPM2 PCR signature which is appended to the image. The image is created under `/efi/EFI/Linux/linux.efi`.
+This script (`create-unified-kernel.sh`) combines the Linux kernel and initrd images and creates a unified image which is then signed for secure boot. The image is created under `/efi/EFI/Linux/linux.efi`.
 
 **The script makes a number of assumptions about the system.** Notably:
 
@@ -11,13 +11,8 @@ This script (`create-unified-kernel.sh`) combines the Linux kernel and initrd im
 * The system has [secure boot keys in `/etc/secureboot`](https://wiki.archlinux.org/title/Unified_Extensible_Firmware_Interface/Secure_Boot#Enrolling_keys_in_firmware)
 * The system has the `sbsigntools` and `binutils` packages installed
 * The system is using the stock kernel (kernel in `/boot/vmlinuz-linux` and initramfs in `/boot/initramfs-linux.img`)
-* The system has the PCR RSA private and public keys at `/etc/systemd/tpm2-pcr-private-key.pem` and `/etc/systemd/tpm2-pcr-public-key.pem` respectively. These can be generated using `openssl`:
-```
-openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -out /etc/systemd/tpm2-pcr-private-key.pem
-openssl rsa -pubout -in /etc/systemd/tpm2-pcr-private-key.pem -out /etc/systemd/tpm2-pcr-public-key.pem
-```
 
-Fortunately, the script is minimal - it's easy to understand and modify to your specific scenario. Specifically, the `VARIANT` variable defined at the top of the script is probably of most interest to you - this can be used to specify custom kernel builds, a la `VARIANT="-zen"`. Other file paths are also provided on top of the file.
+Fortunately, the script is minimal - it's easy to understand and modify to your specific scenario. Specifically, the `VARIANT` variable defined at the top of the script is probably of most interest to you - this can be used to specify custom kernel builds, a la `VARIANT="-zen"`.
 
 You'll also need to actually use `efibootmgr` or similar to create the EFI boot entry. Something like so:
 ```sh
--- create-unified-kernel.sh
+++ create-unified-kernel.sh
@@ -1,66 +1,34 @@
 #!/bin/sh
 
-# kernel variant: -zen, -hardened, etc
 VARIANT=""
-
-# file paths
-LINUX="/boot/vmlinuz-linux${VARIANT}"
-CMDLINE="/etc/cmdline"
-INITRD="/boot/initramfs-linux${VARIANT}.img"
-EFI="/efi/EFI/Linux/linux.efi"
-SB_KEY="/etc/secureboot/keys/db/db.key"
-SB_CERT="/etc/secureboot/keys/db/db.crt"
-PCR_PRIVATE="/etc/systemd/tpm2-pcr-private-key.pem"
-PCR_PUBLIC="/etc/systemd/tpm2-pcr-public-key.pem"
-PCR_SIGNATURE="/etc/systemd/tpm2-pcr-signature.json"
-
-# these are common across different distros, you probably don't need to change these
-OSREL="/usr/lib/os-release"
-EFISTUB="/usr/lib/systemd/boot/efi/linuxx64.efi.stub"
 
 OUTPUT=""
 
 handle_errors() {
-    [ "$1" -eq "0" ] && return
+	[ "$1" -eq "0" ] && return
 
-    echo "$OUTPUT"
-    exit "$1"
+	echo "$OUTPUT"
+	exit "$1"
 }
 
 set +e
 
-OUTPUT+=$(/usr/lib/systemd/systemd-measure sign \
-    --linux="$LINUX" \
-    --osrel="$OSREL" \
-    --cmdline="$CMDLINE" \
-    --initrd=<(cat /boot/*-ucode.img "$INITRD") \
-    --pcrpkey="$PCR_PUBLIC" \
-    --private-key="$PCR_PRIVATE" \
-    --public-key="$PCR_PUBLIC" \
-    --bank=sha1 \
-    --bank=sha256 2>&1)
+mkdir -p /efi/EFI/Linux 2> /dev/null
 
-handle_errors "$?"
-
-printf "%s" "$OUTPUT" > "$PCR_SIGNATURE"
-
-OUTPUT=$(mkdir -p /efi/EFI/Linux && \
-    objcopy \
-        --add-section .osrel="$OSREL" --change-section-vma .osrel=0x20000 \
-        --add-section .cmdline="$CMDLINE" --change-section-vma .cmdline=0x30000 \
-        --add-section .linux="$LINUX" --change-section-vma .linux=0x2000000 \
-        --add-section .initrd=<(cat /boot/*-ucode.img "$INITRD") --change-section-vma .initrd=0x3000000 \
-        --add-section .pcrsig="$PCR_SIGNATURE" --change-section-vma .pcrsig=0x80000 \
-        --add-section .pcrpkey="$PCR_PUBLIC" --change-section-vma .pcrpkey=0x90000 \
-        "$EFISTUB" \
-        "$EFI" 2>&1)
+OUTPUT+=$(objcopy \
+	--add-section .osrel="/usr/lib/os-release" --change-section-vma .osrel=0x20000 \
+	--add-section .cmdline="/etc/cmdline" --change-section-vma .cmdline=0x30000 \
+	--add-section .linux="/boot/vmlinuz-linux${VARIANT}" --change-section-vma .linux=0x2000000 \
+	--add-section .initrd=<(cat /boot/*-ucode.img /boot/initramfs-linux${VARIANT}.img) --change-section-vma .initrd=0x3000000 \
+	"/usr/lib/systemd/boot/efi/linuxx64.efi.stub" \
+	"/efi/EFI/Linux/linux.efi" 2>&1)
 
 handle_errors "$?"
 
 OUTPUT+=$(sbsign \
-    --key "$SB_KEY" \
-    --cert "$SB_CERT" \
-    --output "$EFI" \
-    "$EFI" 2>&1)
+	--key /etc/secureboot/keys/db/db.key \
+	--cert /etc/secureboot/keys/db/db.crt \
+	--output /efi/EFI/Linux/linux.efi \
+	/efi/EFI/Linux/linux.efi 2>&1)
 
 handle_errors "$?"
